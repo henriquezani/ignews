275bce760e189bd8e79faf94393a13953e292e25
henriquezani <henrique.zani@hotmail.com>
2021-12-18 13:26:47 -0300

    in progress

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import styles from '../styles/home.module.scss'
+import Head from 'next/head'
+
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Início | Ignews</title>
+      </Head>
+      <h1 className={styles.title}>Hello world</h1>
+    </>
+  )
+}
